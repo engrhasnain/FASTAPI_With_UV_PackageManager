3d6e7426f82c6f32eb46dc22c8f7cc45f0788064
engrhasnain <pirzadahasnain.18@gmail.com>
2026-01-06 00:19:49 +0500

    Getting Started, Basics Path Parameters

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from bookly!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+# A simple get function simple welcome
+@app.get("/")
+async def health():
+    return "Welcome"
+
+
+# As in below we use f"" to show the username var, we use the r"", one is called 
+# Formatted String Literal and Other is Raw String Literal
+# using path parameter and accepting the name and getting the result back to you like return
+@app.get("/greet/{username}")
+async def greet(username):
+    return f"A warm welcome to {username}"
